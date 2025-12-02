232d47dbae12dc1a6faa7fcfc3ee47d457bac4d0
chao.jin <chao.jin@dbappsecurity.com.cn>
2025-12-02 10:36:50 +0800

    lab1 mapreduce worker part finish

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,11 +1,29 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
+type taskType int
+
+const (
+	Map taskType = iota
+	Reduce
+	Wait
+	Exit
+)
+
+type task struct {
+	filename     string
+	taskID       int
+	taskType     taskType
+	nReduce      int
+	intermediate []string
+}
 
 type Coordinator struct {
 	// Your definitions here.
@@ -14,20 +32,19 @@ type Coordinator struct {
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func AssignTask(arg *ExampleArgs, reply *task) {
 
-//
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +58,23 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
-
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,33 +1,39 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+	"os"
+	"sort"
+	"time"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
@@ -35,14 +41,128 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
+	for {
+		task := getTask()
+		switch task.taskType {
+		case Map:
+			mapper(&task, mapf)
+		case Reduce:
+			reducer(&task, reducef)
+		case Wait:
+			time.Sleep(5 * time.Second)
+		case Exit:
+			return
+		}
+	}
 
 }
 
-//
+func getTask() task {
+	arg := ExampleArgs{}
+	task := task{}
+	call("Coordinator.AssignTask", &arg, &task)
+	return task
+}
+
+func mapper(task *task, mapf func(string, string) []KeyValue) {
+	content, err := os.ReadFile(task.filename)
+	if err != nil {
+		log.Fatalf("Error to ReadFile : %s, err:%v \n", task.filename, err)
+	}
+	kvas := mapf(task.filename, string(content))
+	buckets := make([][]KeyValue, task.nReduce)
+	for _, kv := range kvas {
+		hashID := ihash(kv.Key)
+		buckets[hashID] = append(buckets[hashID], kv)
+	}
+	intermediate := make([]string, 0)
+	for i := 0; i < task.nReduce; i++ {
+		intermediate = append(intermediate, writeKVs2LocalFile(task.taskID, i, buckets[i]))
+	}
+	task.intermediate = intermediate
+	completeTask(task)
+}
+
+func writeKVs2LocalFile(x int, y int, kvs []KeyValue) string {
+	dir, _ := os.Getwd()
+	tmpFile, err := os.CreateTemp(dir, "tmp-*.txt")
+	if err != nil {
+		log.Fatalf("createTemp error in writeKVs2LocalFile: %v\n", err)
+	}
+	enc := json.NewEncoder(tmpFile)
+	for _, kv := range kvs {
+		if err := enc.Encode(kv); err != nil {
+			log.Fatal("Fail")
+		}
+	}
+	outName := fmt.Sprintf("mr-%d-%d", x, y)
+	os.Rename(tmpFile.Name(), outName)
+	return outName
+}
+
+func reducer(task *task, reducef func(string, []string) string) {
+	intermediate := readFiles2KVs(task.intermediate)
+
+	sort.Sort(ByKey(intermediate))
+	dir, _ := os.Getwd()
+	tempFile, err := os.CreateTemp(dir, "mr-tmp-*")
+	if err != nil {
+		log.Fatal("Failed to create temp file", err)
+	}
+	// 这部分代码修改自mrsequential.go
+	i := 0
+	for i < len(intermediate) {
+		//将相同的key放在一起分组合并
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		//交给reducef，拿到结果
+		output := reducef(intermediate[i].Key, values)
+		//写到对应的output文件
+		fmt.Fprintf(tempFile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	tempFile.Close()
+	oname := fmt.Sprintf("mr-out-%d", task.taskID)
+	os.Rename(tempFile.Name(), oname)
+	completeTask(task)
+}
+
+func readFiles2KVs(files []string) []KeyValue {
+	kvs := []KeyValue{}
+
+	for _, filepath := range files {
+		file, err := os.Open(filepath)
+		if err != nil {
+			log.Fatalf("Failed to open file: %v, error:%v\n", file, err)
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kvs = append(kvs, kv)
+		}
+		file.Close()
+	}
+
+	return kvs
+}
+
+func completeTask(task *task) {
+	reply := ExampleReply{}
+	call("Coordinator.CompleteTask", task, &reply)
+}
+
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +187,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
